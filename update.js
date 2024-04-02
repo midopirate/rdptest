255a8e51e5dc321e51ae304e9f163e4fc40cd42a
midopirate <dj.dhriwa2013@gmail.com>
2024-04-03 01:30:47 +0200

    update.js

--- update.js
+++ update.js
@@ -1,1 +1,15 @@
-v5.0.4
+// update.js
+
+// Version information
+const version = "v5.0.3";
+
+// Function to fetch the version
+function getVersion() {
+    return version;
+}
+
+// Export the function
+module.exports = {
+    getVersion
+};
+
